3e662606113814143e268914bbd92f172775b14d
Alexey Milogradov <alexey.milogradov@gmail.com>
2014-11-11 08:13:17 +0200

    Fix settings

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -61,15 +61,17 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-print(dj_database_url.config())
+if dj_database_url.config():
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
